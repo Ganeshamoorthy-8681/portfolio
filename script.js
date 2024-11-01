8d40702553967df7fa7cf79e4c947fa43eab6a5f
Ganeshamoorthy A <ganeshamoorthy8681@gmail.com>
2024-11-01 23:11:38 +0530

    Added the responsiveness to page

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ const observer = new IntersectionObserver((entries, observer) =>
   threshold: 0.1
 });
 
-const targetElements = document.querySelectorAll('.container');
+const targetElements = document.querySelectorAll('.section');
 
 targetElements.forEach(target =>
 {
   observer.observe(target);
 });
+
+
+const navMenuIcon = document.querySelector(".navigation__menu__icon");
+const closeIcon = document.querySelector(".navigation__close__icon");
+const navLinks = document.querySelector(".navigation__list--mobile");
+
+
+function handleNavigationMenuClickEvent()
+{
+  navMenuIcon.classList.toggle("menu-open");
+}
+
+navMenuIcon.addEventListener("click", handleNavigationMenuClickEvent);
+closeIcon.addEventListener("click", handleNavigationMenuClickEvent);
+navLinks.addEventListener("click", handleNavigationMenuClickEvent);
